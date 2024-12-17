7913f225a42a76c8dc82060cf85ef7bdd263f1d2
Junha <yy4124@gmail.com>
2024-12-17 14:29:31 +0900

    유저 엔티티, 레포지토리 추가

---
+++ src/main/java/com/example/session/domain/User.java
@@ -0,0 +1,27 @@
+package com.example.session.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+}
